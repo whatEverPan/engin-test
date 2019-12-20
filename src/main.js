b0be3a9a1b6d312572e07e2c651d9465783aea08
169000417 <1322509208@qq.com>
2019-12-20 12:08:04 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { DatePicker } from 'ant-design-vue';
+Vue.use(DatePicker);
+
 Vue.config.productionTip = false
 
 new Vue({
