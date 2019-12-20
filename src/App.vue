f81e6cca4ba6c860088f13a8cfb9de426663a838
Cat <2454243839@qq.com>
2019-12-20 15:10:21 +0800

    mark

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <div></div>
-    </div>
+      <div>
+        
+      </div>
+    </div> -->
+    <!-- <router-link to="/case" style="color:gray;text-decoration: none;">我的页面</router-link> -->
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -25,9 +28,12 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-align: center
+  
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+  
+} */
 </style>
--- src/components/testCase.vue
+++ src/components/testCase.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-      第一个页面
+  <div class="box">
+    <div class="header">
+      <div class="ziti">
+        <p>测试用例准备</p>
+      </div>
+    </div>
+    <!-- 内容 -->
+    <div class="content">
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.content{
+    width: 925px;
+    height: 490px;
+    background-color: rgba(255, 255, 255, 1);
+    border: 1px solid rgba(204, 204, 204, 1);
+}
+/* 测试用例准备样式 */
+.ziti p {
+  color: #333333;
+  padding: 10px;
+}
+.ziti {
+  border-right: 1px solid rgba(204, 204, 204, 1);
+  border-left: 1px solid rgba(204, 204, 204, 1);
+  border-top: 1px solid rgba(204, 204, 204, 1);
+  text-align: center;
+  width: 180px;
+  height: 54px;
+  background-color: rgba(215, 215, 215, 1);
+}
+.header {
+  width: 926px;
+  height: 53px;
+  background-color: rgba(242, 242, 242, 1);
+}
+/* 外面盒子样式 */
+.box {
+  width: 926px;
+  height: 546px;
+  margin: 20px auto;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
 </style>
